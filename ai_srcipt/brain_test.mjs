a875760e5ed2629bc38ea04b64f03750cdf5588b
ParkerChen <parker.chen@letron.tech>
2025-05-19 18:01:32 +0800

    test

--- ai_srcipt/brain_test.mjs
+++ ai_srcipt/brain_test.mjs
@@ -3,23 +3,27 @@
 // https://guixu.games/index.php/2023/03/28/brain-js%E4%BF%9D%E5%AD%98%E5%B7%B2%E7%BB%8F%E8%AE%AD%E7%BB%83%E5%A5%BD%E7%9A%84%E6%A8%A1%E5%9E%8B/
 // https://www.npmjs.com/package/gpu.js/v/2.4.0#installation
 
+console.log('brain???');
 // https://stackoverflow.com/questions/77251296/distutils-not-found-when-running-npm-install
-import brain from 'brain.js';
+import brain from 'brain.js/dist/brain-browser.min.js';
 // console.log(brain);
-
+console.log('brain');
 const config = {
   binaryThresh: 0.5,
   hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
   activation: 'sigmoid' // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh']
 };
+console.log('config');
 
 const net = new brain.NeuralNetwork(config);
+console.log('net');
 
 net.train([
   { input: { r: 0.03, g: 0.7, b: 0.5 }, output: { black: 1 } },
   { input: { r: 0.16, g: 0.09, b: 0.2 }, output: { white: 1 } },
   { input: { r: 0.5, g: 0.5, b: 1.0 }, output: { white: 1 } }
 ]);
+console.log('net.train')
 
 const output = net.run({ r: 1, g: 0.4, b: 0 });
 console.log(output);
